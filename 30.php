137663663bdb7473e9ab09d89d58000107e19fd4
nickolas1983 <nickolas83@ukr.net>
2016-07-29 09:00:03 +0300

    additional task (done)

--- 30.php
+++ 30.php
@@ -1,7 +1,48 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Администратор
- * Date: 29.07.2016
- * Time: 8:35
- */
+
+$row = 10;
+$cols = 10;
+$colors = array('black','yellow');
+
+echo "<table border = '1'>";
+for ($i = 0; $i < $row; $i++){
+    echo "<tr>";
+    for($j = 0; $j < $cols; $j++){
+        if ($j + $i == 9) echo "<td bgcolor = $colors[0] style=\"width: 20px;\"> </td>";
+        else echo "<td bgcolor = $colors[1] style=\"width: 30px; height: 30px\"> </td>";
+    }
+    echo "</tr>";
+}
+echo "</table><br>";
+
+
+$row = 10;
+$cols = 10;
+$colors = array('black','yellow');
+
+echo "<table border = '1'>";
+for ($i = 0; $i < $row; $i++){
+    echo "<tr>";
+    for($j = 0; $j < $cols; $j++){
+        if ($j == $i) echo "<td bgcolor = $colors[0] style=\"width: 20px;\"> </td>";
+        else echo "<td bgcolor = $colors[1] style=\"width: 30px; height: 30px\"> </td>";
+    }
+    echo "</tr>";
+}
+echo "</table><br>";
+
+
+$row = 10;
+$cols = 10;
+$colors = array('black','yellow');
+
+echo "<table border = '1'>";
+for ($i = 0; $i < $row; $i++){
+    echo "<tr>";
+    for($j = 0; $j < $cols; $j++){
+        if (($j + $i) % 2 == 0) echo "<td bgcolor = $colors[0] style=\"width: 20px;\"> </td>";
+        else echo "<td bgcolor = $colors[1] style=\"width: 30px; height: 30px\"> </td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
